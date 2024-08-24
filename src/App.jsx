bd5b9b4a4ee91456712646577634d3c8ecc9ec2c
mageshkannan <mageshkannanam@gmail.com>
2024-08-24 07:19:46 +0530

    added form in index.html

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,29 @@
 import React from "react";
 
 function App() {
-  return <div>App</div>;
+  return (
+    <form name="contact" method="post">
+      <input type="hidden" name="form-name" value="contact" />
+      <p>
+        <label>
+          Your Name: <input type="text" name="name" />
+        </label>
+      </p>
+      <p>
+        <label>
+          Your Email: <input type="email" name="email" />
+        </label>
+      </p>
+      <p>
+        <label>
+          Message: <textarea name="message"></textarea>
+        </label>
+      </p>
+      <p>
+        <button type="submit">Send</button>
+      </p>
+    </form>
+  );
 }
 
 export default App;
