567bc030390f66a428dc8c90181243c527b6c127
mageshkannan <mageshkannanam@gmail.com>
2024-08-24 06:40:12 +0530

    added form

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,21 @@ import React from "react";
 
 function App() {
   return (
-    <form name="contact" netlify>
+    <form name="contact" method="post">
+      <input type="hidden" name="form-name" value="contact" />
       <p>
         <label>
-          Name <input type="text" name="name" />
+          Your Name: <input type="text" name="name" />
         </label>
       </p>
       <p>
         <label>
-          Email <input type="email" name="email" />
+          Your Email: <input type="email" name="email" />
+        </label>
+      </p>
+      <p>
+        <label>
+          Message: <textarea name="message"></textarea>
         </label>
       </p>
       <p>
