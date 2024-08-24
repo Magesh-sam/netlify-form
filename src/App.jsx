2b8a902b4f8c1fa9a222d6aad9fff1fe53b54eaf
mageshkannan <mageshkannanam@gmail.com>
2024-08-24 07:09:39 +0530

    added form in index.html

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,7 @@
 import React from "react";
 
 function App() {
-  return (
-    <form
-      name="contact"
-      action="/pages/success"
-      method="POST"
-      data-netlify="true"
-    >
-      <input type="hidden" name="form-name" value="contact" />
-      <p>
-        <label>
-          Your Name: <input type="text" name="name" />
-        </label>
-      </p>
-      <p>
-        <label>
-          Your Email: <input type="email" name="email" />
-        </label>
-      </p>
-      <p>
-        <label>
-          Message: <textarea name="message"></textarea>
-        </label>
-      </p>
-      <p>
-        <button type="submit">Send</button>
-      </p>
-    </form>
-  );
+  return <div>App</div>;
 }
 
 export default App;
